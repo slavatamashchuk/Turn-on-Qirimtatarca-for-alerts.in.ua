f9e69febc17de055821b034b076d85003cfe6fb0
Slava Tamashchuk <slava.tamashchuk@gmail.com>
2024-01-27 13:39:21 +0200

    1.1, fixed big bad bug

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const option = document.querySelectorAll('[aria-label="Qırımtatarca"][value="crh-UA"]');
-const select = document.querySelectorAll('[aria-label="Select language"][class="invisible-select"]');
+function enableOptions() {
+    var option = document.querySelectorAll('[aria-label="Qırımtatarca"][value="crh-UA"]');
+    var select = document.querySelectorAll('[aria-label="Select language"][class="invisible-select"]');
+    
+    console.log("Extension started");
+    
+    option.forEach(option => {
+        if (option.hasAttribute('disabled')) {
+            option.removeAttribute('disabled');
+        }
+    });
+}
 
-select.forEach(select => {
-    select.addEventListener('click', () => {
-        option.forEach(option => {
-            if (option.hasAttribute('disabled')) {
-                option.removeAttribute('disabled');
-            }
-        });
+enableOptions();
+
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        if (mutation.type === 'childList') {
+            enableOptions();
+        }
     });
 });
+
+observer.observe(document.body, { childList: true, subtree: true });
